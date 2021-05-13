626cfd81455891b98c4e458a701c22c9c83377bb
zhy <hyzhang228@163.com>
2021-05-13 21:32:12 +0800

    second commit

--- src/main/java/com/xd/git/GitTest.java
+++ src/main/java/com/xd/git/GitTest.java
@@ -3,5 +3,7 @@ package com.xd.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git1");
+        System.out.println("Hello git2");
     }
 }
